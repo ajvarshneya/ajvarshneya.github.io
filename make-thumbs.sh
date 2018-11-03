1d80b6f08adcdce06fafa409a02ff3c987052bf5
A.J. Varshneya <ajvarshneya@gmail.com>
2018-11-03 01:18:16 -0400

    update make thumbs script

--- make-thumbs.sh
+++ make-thumbs.sh
@@ -1,5 +1,10 @@
-for FILE in *-thumb.jpg
-do
-	cp $FILE ${FILE%%.*}-thumb.jpg
-	#sips --resampleWidth 225 $FILE
+for file in *.png; do
+	echo $file
+	cp "$file" ./"${file%%.*}-thumb.png"
 done
+
+
+for file in *-thumb.png; do
+	convert $file -resize 310x310^ -gravity center -extent 310x310 $file
+done
+
