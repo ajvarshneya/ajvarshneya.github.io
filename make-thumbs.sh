a80343dd9dec907825ac6a2929d7113f05bd7433
A.J. Varshneya <ajvarshneya@gmail.com>
2020-02-24 23:50:34 -0500

    update pictures

--- make-thumbs.sh
+++ make-thumbs.sh
@@ -1,10 +1,10 @@
-for file in *.png; do
+for file in *.jpg; do
 	echo $file
-	cp "$file" ./"${file%%.*}-thumb.png"
+	cp "$file" ./"${file%%.*}-thumb.jpg"
 done
 
 
-for file in *-thumb.png; do
+for file in *-thumb.jpg; do
 	convert $file -resize 310x310^ -gravity center -extent 310x310 $file
 done
 
