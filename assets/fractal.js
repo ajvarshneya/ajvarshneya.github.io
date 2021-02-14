205ac4b260a9cd7968d89ce6b557553db6efd59f
ajvarshneya <ajvarshneya@gmail.com>
2021-02-13 23:21:39 -0500

    cool side effect

--- assets/fractal.js
+++ assets/fractal.js
@@ -1,26 +1,69 @@
+// shitty code all rights reserved lol
+// A.J. Varshneya
+
+// Polyfill
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback) {
+       return window.setTimeout(callback, 1000 / 60);
+    });
+}
+
+// Utility
+function toRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function randomRange(min, max) {
+    return Math.random() * (max - min) + min; 
+}
+
+// Drawing lines
+function drawNextLine(start, angle, length, callback) {
+    numLines += 1;
+    const end = nextVertex(start, angle, length);
+    waypoints = getWaypoints(start, end);
+    drawLine(waypoints, 1, callback)
+	return end;
+}
+
+// Gets the next connecting vertex
+function nextVertex(start, angle, length) {
+    return {
+        x: start.x + length * Math.cos(angle), 
+        y: start.y + length * Math.sin(angle)
+    };
+}
+
+// Computes waypoints between start/end for a smooth animation
 function getWaypoints(start, end){
-    let waypoints=[];
-    let dx = end.x - start.x;
-    let dy = end.y - start.y;
-    for(let i = 0 ; i <= TIME_STEPS; i++){
-        let x = start.x + dx * i / TIME_STEPS;
-        let y = start.y + dy * i / TIME_STEPS;
+    const waypoints = [];
+    const dx = end.x - start.x;
+    const dy = end.y - start.y;
+    for(i = 0 ; i <= TICKS_PER_LINE; i++){
+        const x = start.x + dx * i / TICKS_PER_LINE;
+        const y = start.y + dy * i / TICKS_PER_LINE;
         waypoints.push({x: x, y: y});
     }
     return(waypoints);
 }
 
-function animateLine(waypoints, t) {
+// Draws line segment on schedules another on next frame
+// If line is finished, continues with next line or plant (via callback)
+function drawLine(waypoints, t, callback) {
     if (t < waypoints.length - 1) {
-        window.requestAnimationFrame(function() {
-            animateLine(waypoints, t+1)
+        raf = window.requestAnimationFrame(() => {
+            drawLine(waypoints, t+1, callback)
+        });
+    } else {
+        raf = window.requestAnimationFrame(() => {
+            callback();
         });
     }
-    let x1 = waypoints[t-1].x;
-    let y1 = waypoints[t-1].y;
-    let x2 = waypoints[t].x;
-    let y2 = waypoints[t].y;
-    renderLine(x1,y1,x2,y2)
+    const x1 = waypoints[t-1].x;
+    const y1 = waypoints[t-1].y;
+    const x2 = waypoints[t].x;
+    const y2 = waypoints[t].y;
+    renderLine(x1,y1,x2,y2);
 }
 
 function renderLine(x1,y1,x2,y2) {
@@ -30,78 +73,181 @@ function renderLine(x1,y1,x2,y2) {
     context.stroke();
 }
 
-function toRadians(degrees) {
-    return degrees * Math.PI / 180;
-}
-
-function randomRange(min, max) {
-    return Math.random() * (max - min) + min; 
-}
-
-function getBranchChance(depth) {
-    // More branches at low depth and high depth
-    chance = 4 * (depth/MAX_DEPTH - 7/12) ** 2
-    // return 0.40
-    return chance
-}
-
-function drawPlant(start, depth, angle) {
-    if (depth >= MAX_DEPTH) { return }
+// Recursive plant structure
+function drawPlant(start, depth, angle, callback) {
+    deepest = Math.max(depth, deepest);
+    if (stopDrawing) { return }
+    if (depth >= maxDepth) { return }
     if (Math.random() > getBranchChance(depth)) { return }
 
 	depth = depth + 1;
-	let length = MAX_LINE_LENGTH / depth;
+	const length = depth < 3
+        ? maxLineLength / (depth + 2)
+        : maxLineLength / depth;
 	
-    let turnAngle = toRadians(randomRange(TURN_ANGLE_MIN, TURN_ANGLE_MAX));
+    const turnAngle = getTurnAngle(depth);
     
-    start = drawLine(start, angle, length);
-	drawPlant(start, depth, angle + turnAngle);
-    drawPlant(start, depth, angle);
-    
-	start = drawLine(start, angle, length);
-    drawPlant(start, depth, angle + turnAngle);
-    
-    angle -= turnAngle;
-	start = drawLine(start, angle, length);
-	drawPlant(start, depth, angle);
+    // Callback death xD
+    const start1 = drawNextLine(start, angle, length, () => {
+        const start2 = drawNextLine(start1, angle, length, () => {
+            const start3 = drawNextLine(start2, angle - turnAngle, length, () => {
+                drawPlant(start1, depth, angle + turnAngle, () =>  {
+                    drawPlant(start1, depth, angle, () =>  {
+                        drawPlant(start2, depth, angle + turnAngle, () =>  {
+                            drawPlant(start3, depth, angle - turnAngle, () => {});
+                        });
+                    });
+                });
+            });
+        });
+    });
+    callback();
 }
 
-function drawLine(start, angle, length) {
-    let end = nextVertex(start, angle, length);
-    waypoints = getWaypoints(start, end);
-    animateLine(waypoints, 1)
-    numLines += 1;
-	return end;
+// The plants are stochastic and this controls how often to branch
+function getBranchChance(depth) {
+    if (depth == 0) {
+        return 1;
+    }
+    if (depth == 1) {
+        return 1;
+    }
+    if (depth == 2) {
+        return 1;
+    }
+    if (depth == 3) {
+        return 0.8;
+    }
+    if (depth == 4) {
+        return 0.8;
+    }
+    return 7/12;
+    // if (depth == 5) {
+    //     return 0.7;
+    // }
+    // return 0.5;
+    // if (depth == 6) {
+    //     return 0.2;
+    // }
+    // if (depth == 7) {
+    //     return 0.7;
+    // }
+    // if (depth == 8) {
+    //     return 0.2;
+    // }
+    // if (depth == 9) {
+    //     return 0.7;
+    // }
+    // if (depth == 10) {
+    //     return 0.7;
+    // }
+    // if (depth == 11) {
+    //     return 0.7;
+    // }
+    // return 0.50
+
+    // Quadratic so we get more branches at low depth and high depth, fewer at middle depth
+    // return 1/8 * (depth/(2 * MIN_BRANCH_DEPTH) - 1/2) ** 2 + 13/24;
+    
 }
 
-function nextVertex(start, angle, length) {
-    return {
-        x: start.x + length * Math.cos(angle), 
-        y: start.y + length * Math.sin(angle)
-    };
+function getTurnAngle(depth) {
+    // if (depth < 2) {
+    //     const options = [toRadians(randomRange(30, 40)), toRadians(randomRange(-30, -40))]
+    //     return options[Math.floor(randomRange(options.length))]
+    // }
+    return toRadians(randomRange(10, 32));
+    if (depth == 0) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 1) {
+        return toRadians(randomRange(10, 20))
+    }
+    if (depth == 2) {
+        return toRadians(randomRange(15, 20))
+    }
+    if (depth == 3) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 4) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 5) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 6) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 7) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 8) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 9) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 10) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 11) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 12) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 13) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 14) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 15) {
+        return toRadians(randomRange(10, 28))
+    }
+    if (depth == 16) {
+        return toRadians(randomRange(10, 28))
+    }
+    return toRadians(randomRange(10, 28));
 }
 
-const TIME_STEPS = 2;
-const MAX_DEPTH = 20;
 const LINE_COLOR = "#3A5F0B";
-// const LINE_COLOR = "white";
-const TURN_ANGLE_MIN = 10;
-const TURN_ANGLE_MAX = 35;
 
-let canvas = document.getElementById("canvas");
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
-let context = canvas.getContext("2d");
-context.strokeStyle = LINE_COLOR;
-context.lineWidth = "1px";
+const TICKS_PER_LINE = 1;
+const MIN_BRANCH_DEPTH = 10
+const MAX_DEPTH = 20;
 
-const MAX_LINE_LENGTH = Math.sqrt(canvas.clientWidth * canvas.clientHeight) / 13;
-const START_POINT = {x: canvas.clientWidth * 1/2, y: canvas.clientHeight * 1/2};
 const START_ANGLE = toRadians(270);
 const START_DEPTH = 0;
 
 var numLines = 0;
-// while (numLines < 10) {
-    // numLines = 0;
-    drawPlant(START_POINT, START_DEPTH, START_ANGLE);
-// }
+var maxLineLength = 0
+var maxDepth = 0;
+
+var context = null
+var canvas = null
+var raf = null
+var stopDrawing = false
+
+var deepest = 0;
+
+function render() {
+    canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    context = canvas.getContext("2d");
+    context.strokeStyle = LINE_COLOR;
+    context.lineWidth = "1px";
+        
+    maxLineLength = Math.sqrt(canvas.width * canvas.height) / 4;
+    maxDepth = Math.sqrt(canvas.width * canvas.height) / 64;
+
+    let startPoint = {x: 0, y: canvas.height};
+    drawPlant(startPoint, 0 /* depth */, toRadians(235) /* start angle */, function(){});
+
+    startPoint = {x: canvas.width, y: 0};
+    drawPlant(startPoint, 0 /* depth */, toRadians(45) /* start angle */, function(){});
+}
+
+render();
